84c647668b1da1c98f562bdb186f8c2a939ee46f
Abishazaigo <you@example.com>
2021-02-01 19:22:45 +0530

    Added header, form and footer

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Form from './Form';
+import Footer from './Footer';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Form />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
