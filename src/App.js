eef5b9c7a023534a1eff2baf51a07645ee53398b
Abishazaigo <you@example.com>
2021-02-02 19:48:57 +0530

    The login form is connected

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Form from './Form';
 import Footer from './Footer';
+import Login from './Login';
 function App() {
+  const queryClient = new QueryClient();
   return (
     <div className="App">
+      <QueryClientProvider client={queryClient}>
       <Header />
-      <Form />
+      <Login />
       <Footer />
+      </QueryClientProvider>
     </div>
   );
 }
